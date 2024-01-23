2c645cf461afc7fac80df720fd1d18fb4514aab3
LeHan-ZJU <hanlestudy@163.com>
2024-01-23 22:46:40 +0800

    Update DAT_GraphConstraint.py

--- DAT_GraphConstraint.py
+++ DAT_GraphConstraint.py
@@ -7,14 +7,13 @@ import torch
 import torch.nn as nn
 from torch import optim
 from tqdm import tqdm
-import matplotlib.pyplot as plt
 from torch.backends import cudnn
 import torch.distributed as dist
 
 from GCN.gcn import Net
 
 from Models.eval_pose import eval_net_s2_update
-from Models.ContrastModel import ContrastNet1_MultiA, ResSelf_pre
+from Models.ContrastModel import ContrastNet1_MultiA
 from Loss.ContrastiveLoss import self_contrative_loss, self_contrative_meanloss
 
 from utils.dataset_csv import DatasetStage2_iteration
@@ -26,7 +25,7 @@ os.environ['CUDA_VISIBLE_DEVICES'] = '0, 1, 2, 3'
 num_points = 6
 
 resize_w = 640
-resize_h = 480  # 128 64 32 16 8
+resize_h = 480
 extract_list = ["layer4"]
 
 
@@ -41,25 +40,39 @@ def get_args():
     parser.add_argument('--pretrained', default=True, action='store_false')
     parser.add_argument('--anchor_number', '-a', type=int, default=49)
 
-    parser.add_argument('-au', '--augment', dest='augment', type=float, default=0, help='Augmenting or not')
-    parser.add_argument('-an', '--angle', dest='angle', type=float, default=180, help='Angle of self contrastive')
-    parser.add_argument('-e', '--epochs', metavar='E', type=int, default=100, help='Number of epochs', dest='epochs')
-    parser.add_argument('-b', '--batch', metavar='B', dest='batch', type=int, nargs='?', default=6, help='Batch size')
+    parser.add_argument('-au', '--augment', dest='augment', type=float, default=0, 
+                        help='Augmenting or not')
+    parser.add_argument('-an', '--angle', dest='angle', type=float, default=180, 
+                        help='Angle of augment during self contrastive(Graph Constraint)')
+    parser.add_argument('-e', '--epochs', metavar='E', type=int, default=100, 
+                        help='Number of epochs', dest='epochs')
+    parser.add_argument('-b', '--batch', metavar='B', dest='batch', type=int, nargs='?', default=6, 
+                        help='Batch size')
     parser.add_argument('-l', '--learning-rate', metavar='LR', type=float, nargs='?', default=1e-5, dest='lr',
                         help='Learning rate')
-    parser.add_argument('-f', '--load', dest='load', type=str, default=False, help='Load model from a .pth file')
-    parser.add_argument('-w', '--weight', dest='weight', type=float, default=0, help='the weight of similar loss')
-    parser.add_argument('-v', '--validation', dest='val', type=float, default=5.0,
+    parser.add_argument('-f', '--load', dest='load', type=str, default=False, 
+                        help='Load model from a .pth file')
+    parser.add_argument('-w', '--weight', dest='weight', type=float, default=0, 
+                        help='the weight of loss')
+    parser.add_argument('-v', '--validation', dest='val', type=float, default=10.0, 
                         help='Percent of the data that is used as validation (0-100)')
-    parser.add_argument('-s', '--source_label', dest='source_label', type=str, default='', help='Label path of source image label')
-    parser.add_argument('-t', '--target_label', dest='target_label', type=str, default='', help='Label path of source image label')
-    parser.add_argument('-tu', '--target_unlabel', dest='target_unlabel', type=str, default='', help='Label path of source image label')
+    parser.add_argument('-s', '--source_label', dest='source_label', type=str, default='', 
+                        help='Label path of source data')
+    parser.add_argument('-t', '--target_label', dest='target_label', type=str, default='', 
+                        help='Pseudo-label path of target data')
+    parser.add_argument('-tu', '--target_unlabel', dest='target_unlabel', type=str, default='', 
+                        help='Path of target data without pseudo-label')
     parser.add_argument('-i', '--imgs', dest='Imgs', type=str, default='', help='File path of images')
-    parser.add_argument('-pb', '--path_backbone', dest='path_backbone', type=str, default='', help='the path of backbone')
-    parser.add_argument('-pt', '--path_transformer', dest='path_transformer', type=str, default='', help='the path of transformer part')
-    parser.add_argument('-ph', '--path_head', dest='path_head', type=str, default='',  help='the path of head part')
-    parser.add_argument('--graph_model', '-g', default='', metavar='FILE', help="Specify the file in which the model is stored")
-    parser.add_argument('-d', '--dir_checkpoint', dest='ckp', type=str, default='', help='Saved model path')
+    parser.add_argument('-pb', '--path_backbone', dest='path_backbone', type=str, default='', 
+                        help='the path of backbone')
+    parser.add_argument('-pt', '--path_transformer', dest='path_transformer', type=str, default='', 
+                        help='the path of transformer part')
+    parser.add_argument('-ph', '--path_head', dest='path_head', type=str, default='',  
+                        help='the path of head part')
+    parser.add_argument('--graph_model', '-g', default='', metavar='FILE', 
+                        help="Graph model")
+    parser.add_argument('-d', '--dir_checkpoint', dest='ckp', type=str, default='', 
+                        help='Saved model path')
     parser.add_argument("--local_rank", type=int, default=0,
                         help="number of cpu threads to use during batch generation")
     return parser.parse_args()
@@ -111,8 +124,6 @@ def train_net(model,
     criterion_self = self_contrative_meanloss(Graph_model, num_points, device, angle=angle)
 
     val_score_min = 1
-    return_loss_all = []
-    loss_all = np.zeros([4, epochs])
     for epoch in range(30, epochs):
     # for epoch in range(epochs):
         print('epoch:', epoch)
@@ -137,11 +148,7 @@ def train_net(model,
 
                 pred1, pred2 = model(img, img_t)
 
-                loss, return_loss = criterion_self(pred1, pred2, true_heatmaps, true_heatmaps_t, self_supvision)
-                temp = np.zeros(4)
-                temp[0] = epoch + 1
-                temp[1:] = return_loss
-                return_loss_all.append(return_loss)
+                loss = criterion_self(pred1, pred2, true_heatmaps, true_heatmaps_t, self_supvision)
 
                 epoch_loss += loss.item()
                 pbar.set_postfix(**{'loss (batch)': loss.item()})
@@ -168,9 +175,6 @@ def train_net(model,
             print('Best epoch:', epoch + 1)
 
         scheduler.step()
-        print('epoch:', epoch + 1, ' loss:', loss.item())
-        loss_all[0, epoch] = epoch + 1
-        loss_all[1, epoch] = loss.item()
 
         if dist.get_rank() == 0 and (epoch + 1) % 3 == 0:
             try:
@@ -191,8 +195,6 @@ def train_net(model,
         for param_group in optimizer.param_groups:
             print('Lr of optimizer:', param_group['lr'])
 
-    return loss_all  # , np.array(return_loss_all)
-
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
@@ -203,7 +205,6 @@ if __name__ == '__main__':
     torch.cuda.set_device(local_rank)
     global device
     device = torch.device("cuda", local_rank)
-    # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     logging.info(f'Using device {device}')
     print(args.path_backbone)
     print('input_size:', resize_w, resize_h, ';  Augment:', args.augment)
