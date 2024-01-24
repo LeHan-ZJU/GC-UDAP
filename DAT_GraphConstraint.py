6a0412a716437ca73ec8fec512324cf6f0a5fa93
LeHan-ZJU <hanlestudy@163.com>
2024-01-24 22:16:22 +0800

    Update DAT_GraphConstraint.py

--- DAT_GraphConstraint.py
+++ DAT_GraphConstraint.py
@@ -118,7 +118,7 @@ def train_net(model,
         Device:          {Device.type}
     ''')
 
-    optimizer = optim.AdamW(net.parameters(), lr, betas=(0.9, 0.999), eps=1e-08, weight_decay=0, amsgrad=False)
+    optimizer = optim.Adam(net.parameters(), lr, betas=(0.9, 0.999), eps=1e-08, weight_decay=0, amsgrad=False)
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=50, gamma=0.1) 
 
     criterion_self = self_contrative_meanloss(Graph_model, num_points, device, angle=angle)
